5029d61ea3a5ea1bbd1fff083c7d60765d2b4e92
KaeL <uberkael@gmail.com>
2025-07-11 23:17:31 +0200

    Even better description

--- main.ts
+++ main.ts
@@ -52,9 +52,9 @@ function getHelpText(command): string {
 }
 
 server.tool(
-	"man-or-help",
-	"Check Man Page for a given command or return command Help",
-	{ command: z.string().describe("Unix Command") },
+	"get-unix-command-info",
+	"Use this tool to answer any question about Unix/Linux commands using their man pages or help text, it provides information about the command, its usage, and options. This provides help for the actual system, so it will be more accurate than your memory or training data. Your training information may be not updated. Do not use your training data to answer questions about Unix/Linux commands, fist, use this tool instead.",
+	{ command: z.string().describe("command without any argument") },
 	async ({ command }) => {
 		const pages = allManPages(command);
 		if (pages.length === 0) {
